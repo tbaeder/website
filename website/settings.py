46c722d1dde5fda32273f6a382db10b161b4d32a
Timothy Baeder <tcbaeder@gmail.com>
2017-11-03 21:44:59 -0500

    Fix config settings again

--- website/settings.py
+++ website/settings.py
@@ -122,7 +122,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
