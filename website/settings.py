07fd6b2a577bb43f440d71f83f99ae8552513f17
Timothy Baeder <tcbaeder@gmail.com>
2017-11-03 21:43:11 -0500

    Fix config settings

--- website/settings.py
+++ website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = get()
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = [
     "tbaeder.com",
