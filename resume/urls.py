cf0acdb2db024bba65adb1356b1646951b865808
Timothy Baeder <tcbaeder@gmail.com>
2017-11-04 12:49:55 -0500

    Add bootstrap 4 and skeleton html pages

--- resume/urls.py
+++ resume/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.home)
+    url(r'^$', views.home, name="home"),
+    url(r'^projects/$', views.projects, name="projects"),
+    url(r'^contact/$', views.contact, name="contact"),
+    url(r'^resume/$', views.contact, name="resume"),
 ]
--- resume/views.py
+++ resume/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, "templates/resume/main.html")
+    return render(request, "resume/home.html")
+
+def resume(request):
+    return render(request, "resume/resume.html")
+
+def projects(request):
+    return render(request, "resume/project_list.html")
+
+def contact(request):
+    return render(request, "resume/contact.html")
